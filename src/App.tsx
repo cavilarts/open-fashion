82dbaeabd14d131e01c083ea3b92d3697902b4ad
Carlos Avila <cavilart21@gmail.com>
2022-05-10 14:41:29 +0200

    add header component and its unit tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
 
+import { Header } from "./components/Header";
+
 const App: FC = () => {
   return (
     <main>
+      <Header />
       <h1 className="underline text-violet-600">hello</h1>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "tenor-sans": ["Tenor Sans"],
+        sans: ["Tenor Sans"],
+      },
+    },
   },
   plugins: [],
 };
